4627d17f666e9601a511eee1370cb2661d76031e
fletcher0702 <abediernelson@live.fr>
2018-10-12 01:38:39 +0200

    feat: ✨ add create kiosk form

---
+++ src/components/Dashboard/AddKioskForm.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="create-kiosk-container">
+		<h1>Create a kiosk : </h1>
+
+
+		<el-form ref="form" :model="form" label-width="120px">
+
+			<el-form-item label="Activity name">
+				<el-input v-model="form.serialNumber"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="createKiosk">Create</el-button>
+			</el-form-item>
+		</el-form>
+
+	</div>
+</template>
+
+<style>
+
+	.create-kiosk-container{
+
+		margin-left: 20%;
+		width: 50%;
+		padding: 4.5rem;
+	}
+
+
+</style>
+<script>
+
+	export default {
+		name: 'AddKioskForm',
+		data() {
+			return {
+				form: {
+					serialNumber: '',
+				}
+			}
+		},
+		methods: {
+			createKiosk() {
+				const serialNumber = this.form.serialNumber;
+				console.log(serialNumber);
+			}
+		}
+	}
+</script>
---
+++ src/pages/dashboard/kiosk/add/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<el-container>
+		<el-main>
+			<AddKioskForm/>
+		</el-main>
+	</el-container>
+</template>
+
+<script>
+
+	import AddKioskForm from '../../../../components/Dashboard/AddKioskForm';
+	export default {
+		components: {AddKioskForm}
+	}
+</script>
+
+<style scoped>
+
+</style>
