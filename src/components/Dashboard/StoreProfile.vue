80857022096695476e279024c58976aa187031c4
fletcher0702 <abediernelson@live.fr>
2018-10-13 23:46:56 +0200

    feat: ✨ show store profile

---
+++ src/components/Dashboard/StoreProfile.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="infos-container">
+
+		<ul>
+			<li>Name : {{getProfile.name}}</li>
+			<li>Email : {{getProfile.email}}</li>
+			<li>Phone-number : {{getProfile.phoneNumber}}</li>
+		</ul>
+	</div>
+    
+</template>
+
+<script>
+    export default {
+
+        name: "StoreProfile",
+				computed: {
+
+        	getProfile(){
+        		return this.$store.getters.getProfile;
+					}
+				}
+    }
+</script>
+
+<style scoped>
+
+</style>
