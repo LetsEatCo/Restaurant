dab3678c67e68368afdd4b35a02679d61d62f122
ABEDIER <abediernelson@live.fr>
2018-10-29 20:12:11 +0100

    feat: ✨ delete product

--- src/pages/dashboard/products/index.vue
+++ src/pages/dashboard/products/index.vue
@@ -41,6 +41,21 @@
 				fixed="right"
 				label="Operations"
 				width="220">
+
+				<template slot-scope="scope">
+					<!--<el-button-->
+						<!--@click.native.prevent="showEditForm(scope.$index, getProducts)"-->
+						<!--class="Table__delete-button"-->
+						<!--size="small">-->
+						<!--Update-->
+					<!--</el-button>-->
+					<el-button
+						@click.native.prevent="deleteProduct(scope.$index, getProducts)"
+						class="Table__delete-button"
+						size="small">
+						Delete
+					</el-button>
+				</template>
 			</el-table-column>
 		</el-table>
 	</div>
@@ -51,7 +66,8 @@
 	import eventBus from '../../../utils/event-bus';
 	import {
 		STORE_GET_PRODUCTS_REQUEST,
-		STORE_GET_PRODUCTS_REQUEST_SUCCESS
+		STORE_GET_PRODUCTS_REQUEST_SUCCESS,
+		STORE_DELETE_PRODUCT_REQUEST
 	} from '../../../store/actions/store/store.products.actions';
 	import AddProductForm from '../../../components/Dashboard/Product/AddProductForm';
 
@@ -72,7 +88,10 @@
 				eventBus.$on('addProductFormVisible', payload => {
 					this.addFormVisible = !payload;
 				});
-			}
+			},
+			deleteProduct(index, products) {
+				return this.$store.dispatch(STORE_DELETE_PRODUCT_REQUEST, products[index].uuid);
+			},
 		},
 		async asyncData({store}) {
 			const products = await store.dispatch(STORE_GET_PRODUCTS_REQUEST);
