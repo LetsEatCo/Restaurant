3f058fc356430fa0225e1b96620a0e190c57f8b9
Alassane Coly-Arogundade <bonjour@alassanecoly.fr>
2018-10-29 04:02:02 +0100

    feat: Store can view their Products

--- src/pages/dashboard/products/index.vue
+++ src/pages/dashboard/products/index.vue
@@ -4,32 +4,12 @@
 			<h1>Products</h1>
 		</div>
 		<div class="Actions">
-			<el-button class="Actions__add-button" size="medium" @click="addFormVisible = true">Add</el-button>
-
-			<el-dialog title="Edit Ingredient" custom-class="Actions__edit-dialog" :visible.sync="editFormVisible" top="30vh"
-								 width="35%">
-				<el-form ref="form" :model="editForm">
-					<el-form-item label="Ingredient Name">
-						<el-input placeholder="Eg. Salad" v-model="editForm.name" autocomplete="off">{{editForm.name}}</el-input>
-					</el-form-item>
-					<el-form-item label="Quantity">
-						<el-input placeholder="Ingredient quantity available in your stock" v-model="editForm.quantity"
-											autocomplete="off">{{editForm.name}}
-						</el-input>
-					</el-form-item>
-				</el-form>
-				<div slot="footer">
-					<el-button class="cancel-button"
-										 @click="editFormVisible = false">Cancel
-					</el-button>
-					<el-button class="add-button" @click="updateIngredient">Update Ingredient</el-button>
-				</div>
-			</el-dialog>
+			<el-button class="Actions__add-button" size="medium" @click="showAddForm">Add</el-button>
 		</div>
 		<el-table
 			class="Table"
 			ref="multipleTable"
-			:data="this.getIngredients"
+			:data="this.getProducts"
 			stripe
 			style="width: 100%">
 			<el-table-column
@@ -37,91 +17,55 @@
 				width="55">
 			</el-table-column>
 			<el-table-column
+				property="reference"
+				label="Reference">
+			</el-table-column>
+			<el-table-column
 				property="name"
 				label="Name">
 			</el-table-column>
 			<el-table-column
-				property="quantity"
-				label="Quantity">
+				property="price"
+				label="Price (in €)">
 			</el-table-column>
 			<el-table-column
 				fixed="right"
 				label="Operations"
 				width="220">
-				<template slot-scope="scope">
-					<el-button
-						@click.native.prevent="showEditForm(scope.$index, getIngredients)"
-						class="Table__delete-button"
-						size="small">
-						Update
-					</el-button>
-					<el-button
-						@click.native.prevent="deleteIngredient(scope.$index, getIngredients)"
-						class="Table__delete-button"
-						size="small">
-						Delete
-					</el-button>
-				</template>
 			</el-table-column>
 		</el-table>
 	</div>
 </template>
 
 <script>
-	import {
-		STORE_CREATE_INGREDIENT_REQUEST, STORE_DELETE_INGREDIENT_REQUEST,
-		STORE_GET_INGREDIENTS_REQUEST,
-		STORE_GET_INGREDIENTS_REQUEST_SUCCESS, STORE_UPDATE_INGREDIENT_REQUEST
-	} from '../../../store/actions/store/store.ingredients.actions';
 	import {mapGetters} from 'vuex';
-	import _ from 'lodash';
+	import eventBus from '../../../utils/event-bus';
+	import {
+		STORE_GET_PRODUCTS_REQUEST,
+		STORE_GET_PRODUCTS_REQUEST_SUCCESS
+	} from '../../../store/actions/store/store.products.actions';
 
 	export default {
 		layout: 'Dashboard/DashboardLayout',
 		data() {
 			return {
-				addFormVisible: false,
-				editFormVisible: false,
-				form: {
-					name: '',
-					quantity: ''
-				},
-				editForm: {
-					uuid: '',
-					name: '',
-					quantity: ''
-				}
+
 			};
 		},
 		computed: {
-			...mapGetters(['getIngredients'])
+			...mapGetters(['getProducts'])
 		},
 		methods: {
-			addIngredient() {
-				const data = {
-					name: this.form.name,
-					quantity: parseInt(this.form.quantity)
-				};
-				return this.$store.dispatch(STORE_CREATE_INGREDIENT_REQUEST, data);
-			},
-			deleteIngredient(index, ingredients) {
-				return this.$store.dispatch(STORE_DELETE_INGREDIENT_REQUEST, ingredients[index].uuid);
-			},
-			updateIngredient() {
-				this.editForm.quantity = parseInt(this.editForm.quantity);
-				const data = _.pickBy(this.editForm, v => v !== null && v !== undefined && v !== '');
-				return this.$store.dispatch(STORE_UPDATE_INGREDIENT_REQUEST, data);
-			},
-			showEditForm(index, ingredients) {
-				this.editForm.uuid = ingredients[index].uuid;
-				this.editForm.name = ingredients[index].name;
-				this.editForm.quantity = ingredients[index].quantity;
-				this.editFormVisible = true;
+			showAddForm() {
+				this.addFormVisible ? eventBus.$emit('addProductFormVisible', false) : eventBus.$emit('addProductFormVisible', true);
+				eventBus.$on('addProductFormVisible', payload => {
+					this.addFormVisible = !payload;
+				});
 			}
 		},
 		async asyncData({store}) {
-			const ingredients = await store.dispatch(STORE_GET_INGREDIENTS_REQUEST);
-			store.commit(STORE_GET_INGREDIENTS_REQUEST_SUCCESS, ingredients);
+			const products = await store.dispatch(STORE_GET_PRODUCTS_REQUEST);
+			store.commit(STORE_GET_PRODUCTS_REQUEST_SUCCESS, products);
 		}
 	};
 </script>
--- src/store/modules/store/index.js
+++ src/store/modules/store/index.js
@@ -1,3 +1,4 @@
 export {storeModule} from './store.module';
 export {storeKiosksModule} from './store.kiosks.module';
 export {storeIngredientsModule} from './store.ingredients.module';
+export {storeProductsModule} from './store.products.module';
